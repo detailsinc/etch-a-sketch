64dd382f08ee09843243b86a0e12cd81cda518fc
Erick Robles <76699626+detailsinc@users.noreply.github.com>
2021-04-25 03:54:38 -0700

    Added two functions and mouse over
    Added GetNumInput(), getRandomColors(), and mouse over event for project

--- etchASketch.js
+++ etchASketch.js
@@ -3,27 +3,55 @@ const section = document.querySelector('section');
 const button = document.querySelector('#title');
 
 
-//Generate 16x16 grid
+//Generate grid
 function generateGrid() {
+    clearGrid();
     let div;
-    for (let i = 0; i < 256; i++) {
+    let numInput = getNumInput();
+    let gridSize = numInput * numInput;
+    section.style.cssText = `grid-template-columns: repeat(${numInput}, 1fr); grid-template-rows: repeat(${numInput}, 1fr)`;
+
+    //Loop creating divs
+    for (let i = 0; i < gridSize; i++) {
         div = document.createElement('div');
         div.classList.add('tile');
         section.appendChild(div);  
-    }    
+    }
+    
 }
 
-//Delete and Replace element
-function swapButtonToText() {
-    button.remove();
-    let title = document.createElement('h1');
-    let body = document.querySelector('body'); 
-    title.textContent = "Etch A Sketch";
-    body.insertBefore(title, section);
+//Function to clear Grid
+function clearGrid() {
+    const oldArray = Array.from(section.childNodes);
+    oldArray.forEach((div) => {
+        section.removeChild(div);
+    })
+
 }
 
-//Button to begin etch a sketch
+//Function to get userInput
+function getNumInput() {
+    let userNumInput = prompt('Enter a number between 1-64');
+    let num = Number.parseInt(userNumInput);
+    if (isNaN(num) || num < 1 || num > 64) {
+        alert('Error: Please enter a number between 1-64'); 
+    } else {
+        return(num);
+    } 
+}
+
+//Random Color Function
+function getRandomColors(e) {
+    const randomRed = Math.floor(Math.random() * 256);
+    const randomBlue = Math.floor(Math.random() * 256);
+    const randomGreen = Math.floor(Math.random() * 256);
+    e.target.style.backgroundColor = `rgb(${randomRed}, ${randomGreen}, ${randomBlue})`;
+}
+
+//Button to change grid size 1-64
 button.addEventListener('click', () => {
     generateGrid();
-    swapButtonToText()
 }) 
+
+//On mouse over event
+section.addEventListener('mouseover', getRandomColors);
